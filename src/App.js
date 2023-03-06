d55597b304accdfe8096180171aec0bc13b54191
leesh5000 <leesh50000@gmail.com>
2023-03-07 08:52:03 +0900

    #69 - 개인정보 이용방침 페이지 개발

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import QuizListPage from "./pages/quiz/QuizListPage";
 import OAuth2RedirectHandler from "./components/social/OAuth2RedirectHandler";
 import PostQuizPage from "./pages/post/PostQuizPage";
-import Home from "./pages/Home";
+import Home from "./pages/common/Home";
 import QuizDetailPage from "./pages/quiz/QuizDetailPage";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/common/LoginPage";
 import Auth from "./lib/hoc/Auth";
 import UserProfilePage from "./pages/user/UserProfilePage";
 import UserQuizListPage from "./pages/user/UserQuizListPage";
 import UserAnswerListPage from "./pages/user/UserAnswerListPage";
+import Privacy from "./pages/common/Privacy";
 
 function App() {
 
@@ -29,6 +30,7 @@ function App() {
       <Route path="/users/:id" element={<AuthUserProfilePage/>} />
       <Route path="/users/:id/quizzes" element={<AuthQuizListPage/>} />
       <Route path="/users/:id/answers" element={<AuthAnswerListPage/>} />
+      <Route path="/privacy" element={<Privacy/>} />
       <Route path="/*" element={<Home />} />
     </Routes>
   )
