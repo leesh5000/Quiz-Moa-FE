60b9c54de1d56626acac46e0a60b33a0551783fa
leesh5000 <leesh50000@gmail.com>
2023-03-09 07:20:30 +0900

    #71 - 텍스트 색상 버튼 추가

--- src/components/answer/AnswerEditor.js
+++ src/components/answer/AnswerEditor.js
@@ -50,11 +50,20 @@ const AnswerEditor = ({quillElement, quillInstance, user}) => {
       modules: {
         // https://quilljs.com/docs/modules/toolbar/ 참고
         toolbar: [
-          [{header: '1'}, {header: '2'}],
-          ['bold', 'italic', 'underline', 'strike'],
-          [{list: 'ordered'}, {list: 'bullet'}],
-          // ['blockquote', 'code-block', 'link', 'image'],
-          ['blockquote', 'code-block', 'link'],
+          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+          ['blockquote', 'code-block'],
+
+          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+
+          ['clean']                                         // remove formatting button
         ],
       },
     });
